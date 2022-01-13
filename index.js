a26c9ed6eb5ece85af36ef2622e8b15b503a02dc
TrueBabyChaise <tomaugier23@gmail.com>
2022-01-13 12:20:31 +0100

    revert

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-var back = 1
+
 
 window.onload = () => {
-
-	document.getElementById("bodyChange").onclick = function(e) {
-		console.log(e)
-		
-		textZone = document.getElementById("textzone")
-
-		p = document.createElement("p")
-		p.innerText = back ? 'yes' : 'no'
-		back = back ? 0 : 1
-		textZone.innerHTML = ""
-		textZone.appendChild(p)
-	}
 }
